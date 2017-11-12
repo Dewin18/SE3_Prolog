2755ab67fff20112acfbae0c101869649c8630a1
Max <wutzmax@googlemail.com>
2017-11-12 22:19:58 +0100

    Some more work on Ex1 and Ex3

--- 02/Blatt02_DeMaCh.pl
+++ 02/Blatt02_DeMaCh.pl
@@ -9,28 +9,45 @@
 
 %Aufgabe 1
 
-/*
+%1.1
 
-Prädikat:
+%Prädikat:
+%
+%Ist eine Menge von Klauseln mit gleicher Signatur. Also Klauseln deren Köpfe den gleichen Funktor haben.
+%
+%zB.:
+%verbrecher(X) :- mann(X)
+%mann(X) :- mensch(X), maennlich (X), erwachsen (X)
+%
+%Klausel:
+%
+%Sind die Generalisierung von Fakten und Regeln, die Stelligkeit bzw. die Menge der Parameter werden in einem '/' hinter der Klausel angegeben 
+%
+%zB.: mutter_von/2
+%
+%Struktur:
+%
+%Besteht aus einem Namen und mindestens einem Argument. Jedes Argument ist ein Term.
+%
+%zB.:
+%hoerspiel(titel('Qualityland'), autor('Marc-Uwe Kling'))
 
-Ist eine Menge von Klauseln mit gleicher Signatur. Also Klauseln deren Köpfe den gleichen Funktor haben.
+%1.2
 
-zB.:
-verbrecher(X) :- mann(X)
-mann(X) :- mensch(X), maennlich (X), erwachsen (X)
-
-Klausel:
-
-Sind die Generalisierung von Fakten und Regeln, die Stelligkeit bzw. die Menge der Parameter werden in einem '/' hinter der Klausel angegeben (zB.: mutter_von/2)
-
-Struktur:
-
-Besteht aus einem Namen und mindestens einem Argument. Jedes Argument ist ein Term.
-
-zB.:
-hoerspiel(titel('Qualityland'), autor('Marc-Uwe Kling'))
-
-*/
+%Suche:
+%
+%Die Suche ist bei Prolog ein besonders interessantes Thema da die Datenstrukturen ja meist in Relationen aufgebaut sind und somit 
+%besonders einfach und besonders schön durchsucht bzw. zugeordnet werden können
+%
+%Variable:
+%
+%Auch hier eine Besonderheit von Prolog, Variablen haben keinen vorgeschriebenen Datentyp. Es muss also nicht begrenzt werden was gespeichert 
+%bzw adressiert werden kann, genau so wie es keine (sichtbaren) Pointer gibt.
+%
+%Instanziierung:
+%
+%Man kann beim ausführen eines Prädikates Variablen mitgeben, je nachdem ob man einen Wert oder einen Platzhalter mitgibt, reagiert das 
+%Prädikat teilweise unterschiedlich. Vollständig instanziiert, vollständig uninstanziiert, teilweise instanziiert.
 
 %Aufgabe 2
 
@@ -76,14 +93,21 @@ hoerspiel(titel('Qualityland'), autor('Marc-Uwe Kling'))
 
 %3.1
 
-idnameconvert(Id,Name):-findall(file(Id,_,Name,_,_,_).
+fileidnamecon(FileId,Name):-file(FileId,_,Name,_,_,_).
 
-%geht nur wenn's eindeutig ist, daher hier eine etwas bessere Lösung die so 
-%noch nicht ganz funktioniert
-
-idnameconvert(Id,Name,Output):-findall(Id,file(Id,_,Name,_,_,_),Output).
+%geht nur wenn's eindeutig ist, weiss nicht genau was mit alternativer Variablenbindung gemeint ist?
 
 %3.2
+
+directoryidnamecon(DirId,Name):-directory(DirId,Name,_,_,_).
+
+%das gleiche wie bei 3.1
+
 %3.3
+
+filenametodirinfo(FileName,DirName):-file(_,DirId,FileName,_,_,_),directory(DirId,DirName,_,_,_).
+
+%gibt nur den Ordnernamen aus
+
 %3.4
 
