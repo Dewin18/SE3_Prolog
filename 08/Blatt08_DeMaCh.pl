9da29d1b8ad954b5b9ba62cdd95d859eced7c783
Dewin <dewin18@live.de>
2017-12-13 16:45:03 +0100

    alles von 2.1 -  2.6

--- 08/Blatt08_DeMaCh.pl
+++ 08/Blatt08_DeMaCh.pl
@@ -22,7 +22,7 @@ head(E,[E|_]).
 %Gibt die Restliste zurueck und entfernt den Listenkopf
 tail([_|T], T). 
 
-%Prüft, ob eine Binärzahl gerade ist
+%Prüft, ob eine Binaerzahl gerade ist
 %bin_even(+BinaryList)
 bin_even([]).
 bin_even(L) :- 
@@ -40,7 +40,7 @@ bin_even(L) :-
 %?- bin_even([1,1,1]).
 %false.
 
-%Prüft, ob eine Binärzahl ungerade ist
+%Prüft, ob eine Binaerzahl ungerade ist
 %bin_odd(+BinaryList)
 bin_odd(L) :- 
 	head(LSB, L),
@@ -58,7 +58,7 @@ bin_odd(L) :-
 %true.
 
 /*2.2*/
-%Verdoppelt eine Binärzahl
+%Verdoppelt eine Binaerzahl
 %bin_double(?BinaryList, ?Doubled)
 bin_double([0], [0]).
 bin_double(L, Doubled) :-
@@ -66,9 +66,174 @@ bin_double(L, Doubled) :-
 	
 %Ausgaben:
 
-%Halbiert eine Binärzahl, wenn sie gerade ist
+%?- bin_double([1], L).
+%L = [0, 1].
+
+%?- bin_double([1, 0, 0, 1], L).
+%L = [0, 1, 0, 0, 1].
+
+%Halbiert eine Binaerzahl, wenn sie gerade ist
 %bin_half(?BinaryList, ?Half)
 bin_half(L, Half) :-
 	bin_double(Half, L).
 	
 %Ausgaben:	
+
+%?- bin_half([1], L).
+%false.
+
+%?- bin_half([0, 0, 1], L).
+%L = [0, 1]
+
+/*2.3*/
+
+%Ueberfuehrt eine ungerade Binaerzahl in eine gerade, durch subtraktion von 1
+%make_bin_even(+BinaryList, -EvenBinaryList)
+make_bin_even([1], [0]).
+make_bin_even(L, Even) :-
+	bin_odd(L),
+	tail(L, Tail),
+	Even = [0|Tail].
+
+%Ausgaben:	
+	
+%?- make_bin_even([1, 1], L).
+%L = [0, 1].
+
+%?- make_bin_even([0, 1, 1, 1, 1], L).
+%false.
+
+/*2.4*/
+
+%Prüft, ob eine Binaerzahl vorliegt
+%is_bin(+BinaryList)
+is_bin([LSB]) :- LSB =< 1.
+is_bin(L) :- 
+	length(L, Length),
+	Length > 0,
+	head(LSB, L),
+	LSB =< 1,
+	tail(L, Tail),
+	is_bin(Tail).
+
+%Ausgaben
+
+%?- is_bin([]).
+%false.
+
+%?- is_bin([1]).
+%true .
+
+%?- is_bin([0]).
+%true .
+
+%?- is_bin([0, 1, 0, 1, 1]).
+%true .
+
+%?- is_bin([0, 1, 0, 9, 1]).
+%false.
+
+%?- is_bin([0, 1, 0, 0]).
+%true .
+
+/*2.4 Bonus*/
+
+%Prüft, ob eine Binaerzahl vorliegt, die keine fuehrenden Nullen enthaelt.
+%is_bin_bonus(+BinaryList)
+is_bin_bonus([0]) :- true.
+is_bin_bonus(L) :-
+	reverse(L, L2),
+	head(LSB, L2),
+	LSB =\= 0,
+	is_bin(L).
+
+%Ausgaben:
+
+%?- is_bin_bonus([0]).
+%true .
+
+%?- is_bin_bonus([1]).
+%true .
+
+%?- is_bin_bonus([1, 0, 1, 0, 1]).
+%true .
+
+%?- is_bin_bonus([1, 0, 1, 1, 0]).
+%false.
+
+%?- is_bin_bonus([1, 0, 1, 9, 1]).
+%false.
+
+/*2.5*/
+%Wandelt eine Binaerzahl in eine Natuerliche Zahl um
+%binToInt(+BinaryList, +Nat)
+binToInt(L, Int) :-
+	convertBin(L, 1, Int).
+
+%Hilfsprädikat für die Umrechnung
+%convertBin(+BinaryList, +ExpoSequence, -Nat)	
+convertBin([1], C, C).	
+convertBin([0], 1, 0). 
+convertBin(L, C, N) :-	
+	length(L, Length), 
+	Length > 0,			
+	head(LSB, L),					%Wir holen uns den Listenkopf LSB
+    tail(L, Tail),					%Die liste wird um den Listenkopf verringert
+	TempResult is  C * LSB,			%Das Zwischenergebnis ist am Anfang 1 oder 0, da LSB 1 oder 0 ist und C = 1 am Anfang gilt
+	NextC is 2 * C,					%C wird nun verdoppelt und hat im nächsten rek. Aufruf den Wert 2
+	convertBin(Tail, NextC, N2),	%Rekursiver Aufruf mit der Restliste, 2, und N2
+	N is N2 + TempResult.			%Zuletzt werden alle Zwischenergebnisse addiert. 
+
+%Ausgaben
+
+%?- binToInt([0], X).
+%X = 0 ;
+%false.
+
+%?- binToInt([1], X).
+%X = 1 ;
+%false.
+	
+%?- binToInt([1, 0, 1], X).
+%X = 5 ;
+%false.
+
+%?- binToInt([1, 0, 1, 0, 1, 0, 1, 0, 1, 1], X).
+%X = 853 ;
+%false.
+
+%?- binToInt([1, 0, 1, 0, 1, 0], X).
+%false.	
+
+%Wandelt eine Natuerliche Zahl in eine Binaerzahl um
+%intToBin(+Integer, -BinaryList)
+intToBin(0, []).		
+intToBin(Int, L) :-
+	Int > 0,
+	Rem is Int / 2,
+	DivRem is floor(Rem),
+	ModRem is Int mod 2,
+	intToBin(DivRem, L2),
+	append([ModRem], L2, L).
+
+%Ausgaben
+
+%?- intToBin(1, X).
+%X = [1] ;
+%false.	
+
+%?- intToBin(2, X).
+%X = [0, 1] ;
+%false.	
+
+%?- intToBin(255, X).
+%X = [1, 1, 1, 1, 1, 1, 1, 1] 
+%false.
+
+%?- intToBin(256, X).
+%X = [0, 0, 0, 0, 0, 0, 0, 0, 1] 
+%false.
+
+/*2.6*/
+
+%TODO
