d3e3d553695992963c0441af72e15c577a7ec6b1
Dewin <dewin18@live.de>
2017-12-18 00:29:00 +0100

    ex. 2.7

--- 08/Blatt08_DeMaCh.pl
+++ 08/Blatt08_DeMaCh.pl
@@ -68,7 +68,11 @@ head(E,[E|_]).
 %Gibt die Restliste zurueck und entfernt den Listenkopf
 tail([_|T], T). 
 
-%Prüft, ob eine Binaerzahl gerade ist
+%Entfernt das letzte Element aus einer Liste und gibt die Subliste zurueck
+remove_last([_], []).
+remove_last([X|Xs], [X|Removed]) :- remove_last(Xs, Removed).
+
+%Prueft, ob eine Binaerzahl gerade ist
 %bin_even(+BinaryList)
 bin_even([]).
 bin_even(L) :- 
@@ -151,7 +155,7 @@ make_bin_even(L, Even) :-
 
 /*2.4*/
 
-%Prüft, ob eine Binaerzahl vorliegt
+%Prueft, ob eine Binaerzahl vorliegt
 %is_bin(+BinaryList)
 is_bin([LSB]) :- LSB =< 1.
 is_bin(L) :- 
@@ -285,7 +289,7 @@ intToBin(Int, L) :-
 /*Die Definition ist extensional, da die gesamte "Berechnung" in Form
 von Fakten definiert wurde.*/
 
-%Addiert zwei Binaerzahlen 
+%Addiert zwei Binaerzahlen gemaess Volladierer
 %add_bin(+Binary1, +Binary2, ?Cin, ?Cout, -Sum)
 add_bin(0, 0, 0, 0, 0).
 add_bin(0, 0, 1, 0, 1).
@@ -297,29 +301,73 @@ add_bin(1, 1, 0, 1, 0).
 add_bin(1, 1, 1, 1, 1).
 
 /*2.7*/
-add_bin_large(L1, L2, Result) :-
-	StartCin is 0,
-	add_bin_lists(L1, L2, StartCin, _, Result).
+%Berechnet die Summer beliebig langer Binaerzahlen
+%add_large_bin(+BinaryList1, +BinaryList2, -Sum)
+add_large_bin(L1, L2, Result) :-
+	length(L1, Length1),                 %Laenge der ersten Binaerliste bestimmen
+	length(L2, Length2),			     %Laenge der zweiten Binaerliste bestimmen
+	Max is max(Length1, Length2),        %Maximale Listenlaenge bestimmen
+	adapt_length(L1, Max, EL1),          %Binaerliste1 evtl. auf maximale Laenge erweitern
+	adapt_length(L2, Max, EL2),          %Binaerliste2 evtl. auf maximale Laenge erweitern
+	getSum(EL1, EL2, 0, Cout, R1),       %Summe mit dem Praedikat getSum berechnen
+	removeLeadingZero(R1, Cout, Result). %Entfernt eine moegliche fuehrende Null (der letzte Cout von getSum/5)
 
-%%TODO	
-	
-add_bin_lists([], [], Cin, _, [Cin]).	
-add_bin_lists(L1, L2, Cin, Cout, S) :-
+%Hilfspraedikat um eine Liste Max lang zu machen, damit eine Addition via Volladierer moeglich ist
+%adapt_length(+BinaryList, +MaxLength, -NewList)
+adapt_length(L1, Max, L1) :- length(L1, Length), Length =:= Max.
+adapt_length(L1, Max, NewList) :-
+	length(L1, Length),
+	Length < Max,
+	Difference is Max - Length,
+	getZeroList(Difference, ZeroList),
+	append(L1, ZeroList, NewList).
+
+%Gibt eine Liste der groesse N zurueck, bei der jedes Element 0 ist.
+%getZeroList(+NumberOfElments, -List) 
+getZeroList(0, []).
+getZeroList(N, L) :-
+	N > 0,
+	N1 is N - 1,
+	getZeroList(N1, L2),
+	append(L2, [0], L).
+
+%Berechnung der Summe in der gesamten Liste fuer jeweils zwei Bits 
+%getSum(+BinaryList1, +BinaryList2, ?Cin, ?Cout, -Sum)
+getSum([], [], Cin, Cin, [Cin]).	
+getSum(L1, L2, Cin, Cout, S) :-
 		head(H1, L1),
 		head(H2, L2),
-		add_bin(H1, H2, TempCin, X, TempS),
+		add_bin(H1, H2, Cin, Cout2, S2),
 		tail(L1, T1),
 		tail(L2, T2),
-		add_bin_lists(T1, T2, TempCout, _, S2),	
-		TempCout = TempCin,
-		append([TempS], S2, S). 
+		getSum(T1, T2, Cout2, Cout3, S3),	
+		Cout is Cout3,
+		append([S2], S3, S). 	
 
+%Hilfspraedikat um die fuehrende 0 aus einer Liste zu entfernen
+%removeLeadingZero(+BinaryList, +Cout, -NewList)		
+removeLeadingZero(L, Cout, L) :- Cout =:= 1.		
+removeLeadingZero(L, Cout, L1) :-
+	Cout =:= 0,
+	remove_last(L, L1).
 		
+
 %Und noch mal eine andere, leichtere Variante :P
-%addBinCool(+BinaryList, +BinaryList, -Result)
+%addBinCool(+BinaryList1, +BinaryList2, -Result)
 add_bin_cool(L1, L2, Result) :-
 	binToInt(L1, N1),
 	binToInt(L2, N2),
 	intToBin((N1 + N2), Result).
 
-%TODO
+/*2.8*/ %TODO
+
+mult_bin([1], M2, M2).
+mult_bin(M1, M2, []) :- bin_even(M1).
+mult_bin(M1, M2, Result) :-
+	bin_half(M1, Half),
+	bin_double(M2, Doubled),
+	mult_bin(Half, Doubled, R2),
+	add_large_bin(Half, Doubled, R3),
+	add_large_bin(R2, R3, Result).
+	
+	
