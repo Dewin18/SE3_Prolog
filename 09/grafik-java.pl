baf5453c48fcd85d4108413cd2b318e1c5f8a749
Dewin <dewin18@live.de>
2018-01-03 15:34:13 +0100

    ex. 2.2 done

--- 09/grafik-java.pl
+++ 09/grafik-java.pl
@@ -1,52 +1,49 @@
 :- use_module(library(jpl)).
 
 demo_draw :-
-	jpl_new('ShapeDrawingFrame', ['*** MyTitleHere ***'], Frame),
-	jpl_call(Frame, setSize, [1000, 1200], _),
+	jpl_new('ShapeDrawingFrame', ['PROLOG - RECURSIVE CIRCLE'], Frame),
+	jpl_call(Frame, setSize, [620, 620], _),
 
 	add_black_background(Frame),
-
-	draw_object(Frame, 400, 300),
+	
+	draw_object(Frame, circle, 500, green, 4, 50, 40),
+	draw_object(Frame, circle, 500, red, 20, 50, 40),
 
 	% display Frame
 	jpl_call(Frame, setVisible, [@(true)], _),
 
 	save_drawing(Frame).
 
-% +Frame: der ShapeDrawingFrame dem ein schwarzer Hintergrund hinzugefÃ¼gt werden soll
-% beispielhaftes HinzufÃ¼gen einer Shape zu einem ShapeDrawingFrame.
+%add_black_background(+Frame)
 add_black_background(Frame) :-
-	% GrÃ¶ÃŸe des Frames herausfinden
+    getShape(rectangle, Shape),
 	jpl_call(Frame, getSize, [], Dimension),
-	% Ausdehnung von Dimension auslesen
 	jpl_get(Dimension, height, Height),
-	% ... alternativ getter benutzen
 	jpl_call(Dimension, getWidth, [], Width),
-	% beachten Sie, dass innere Klassen nicht mit einem Punkt als Trenner, 
-	% sondern mit einem Dollarzeichen getrennt werden: im Paket java.awt.geom
-	% gibt es eine Klasse Rectangle2D mit einer inneren Klasse Float.
-	% Ã„hnliches gilt fÃ¼r viele Klassen im Geometry-Paket.
-	jpl_new('java.awt.geom.Rectangle2D$Float', [0, 0, Width, Height], Rectangle),
-	% java.awt.Color enthÃ¤lt einige vordefinierte Farben:
+	jpl_new(Shape, [0, 0, Width, Height], Rectangle),
 	jpl_get('java.awt.Color', black, Black),
 	jpl_call(Frame, addFillShape, [Rectangle, Black], _).
 
-% draw_object(Frame,Size,CurrentSize,*** add additional parameters here, if needed ***)
-% recursively adds some shapes of size Size to Frame
-% CurrentSize is decreased recursively fom Size to 0
-draw_object(_,_,0 /* , *** add additional parameters here, if needed *** */). 
-draw_object(Name,Size,CSize /* , *** add additional parameters here, if needed *** */) :- 
-   CSize > 0 ,        % only for positive integers
-
-  % *** insert the computation of graphical parameters here ***
-
-  % *** create and add the current graphical object here ***
-
-  % decrement CurrentSize and call draw_object recursively
-  CSizeNew is CSize - 2,
-% writeln(CSizeNew),
-  draw_object(Name,Size,CSizeNew).
-
+%returns the class for a rectangle or ellipse
+%getShape(+ShapeName, -Shape)
+getShape(ShapeName, Shape) :- Shape = 'java.awt.geom.Rectangle2D$Float', ShapeName = rectangle.
+getShape(ShapeName, Shape) :- Shape = 'java.awt.geom.Ellipse2D$Float', ShapeName = circle. 	
+	
+%Draws an object in a recursive way. The recursion factor determines the number of recursion steps. 
+%High recursion factor means less recursion steps.
+%draw_object(+Frame, +ShapeName, +Size, +Color, +RecursionFactor, +X, +Y)
+draw_object(_, _, Size, _, _, _, _) :- Size =< 0.	
+draw_object(Name, ShapeName, Size, CurrentColor, RFactor, X, Y) :- 
+  getShape(ShapeName, Shape),
+  Size > 0,    
+  SizeNew is Size - (2 * RFactor),
+  jpl_new(Shape, [X, Y, Size, Size], Rectangle),
+  jpl_get('java.awt.Color', CurrentColor, Color),
+  jpl_call(Name, addDrawShape, [Rectangle, Color], _),
+  NewX is X + RFactor,
+  NewY is Y + RFactor,
+  draw_object(Name, ShapeName, SizeNew, CurrentColor, RFactor, NewX, NewY).
+  
 
 % save drawing in the frame (if requested by user)
 save_drawing(Frame):-
