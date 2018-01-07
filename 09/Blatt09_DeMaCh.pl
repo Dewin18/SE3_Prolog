713c254ef2b3c4219af102c911db8f922aab938c
Max <wutzmax@googlemail.com>
2018-01-07 17:58:29 +0100

    chatbot

--- 09/Blatt09_DeMaCh.pl
+++ 09/Blatt09_DeMaCh.pl
@@ -10,3 +10,22 @@ Datum 19.12.2017
 
 /*Aufgabe 1: Chat-Bots*/
 
+chatbot(Pattern,Response) :-
+	rule(TempPattern, TempResponse),
+	match(TempPattern, Pattern),
+	match(TempResponse, Response),
+	!.
+	
+rule([s([ich,habe]),s(Menge),s(Art),w(gegessen),_],[s([warum,hast,du]),s(Menge),s(Art),w(gegessen),w('?')]).
+
+
+match([],[]).
+match([Item|Items],[Word|Words]) :-
+	match(Item, Items, Word, Words).
+
+match(w(Word), Items, Word, Words) :-
+	match(Items, Words).
+
+match(s([Word|Seg]), Items, Word, Words0) :-
+	append(Seg, Words1, Words0),
+	match(Items, Words1).
