f0ff4cb7b6a5da1155f92a2fc1517ec7750bd113
Dewin <dewin18@live.de>
2018-01-10 11:13:50 +0100

    ex 1

--- 10/Blatt10_DeMaCh.pl
+++ 10/Blatt10_DeMaCh.pl
@@ -20,26 +20,24 @@ tail([_|T], T).
 
 get_date_list(L2) :- 
 	findall(Date, dax(Date, _,_), L), 
-	reverse(L, L2).
+	reverse(L, L2).´
 
 print_all_courses(CList) :-
 	get_date_list(L),
-	get_all_courses(L, [], CList).
-	%display("DAX", Clist).
-	
-get_all_courses(L,[], _) :- 
 	length(L, Length),
-	Length =< 0.
+	get_all_courses(L, Length, CList),
+	display("DAX", CList).
 	
-get_all_courses(L, EList, Clist) :-
-	length(L, Length),
+get_all_courses(_, 0, []). 
+get_all_courses(L, Length, CList) :-
 	Length > 0,
+	NewLength is Length - 1,
 	head(Head, L),
 	tail(L, Tail),
 	dax(Head, EKurs, SKurs),
 	append([EKurs], [SKurs], DaxPart),
-	append(EList, DaxPart, Newlist),
-	Clist = NewList,
-	get_all_courses(Tail, NewList, _).
+	get_all_courses(Tail, NewLength, NewList),
+	append(NewList, DaxPart, XList),
+	CList = XList.
 	
 	
