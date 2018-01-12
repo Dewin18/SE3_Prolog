18b7f3d4aa5803695f68731c1ad25bef12a57491
Dewin <dewin18@live.de>
2018-01-12 17:44:19 +0100

    ex 2 done.

--- 10/Blatt10_DeMaCh.pl
+++ 10/Blatt10_DeMaCh.pl
@@ -22,27 +22,45 @@ get_date_list(L2) :-
 	findall(Date, dax(Date, _,_), L), 
 	reverse(L, L2).
 
-print_all_courses(CList) :-
+%1.1
+print_all_courses() :-
 	get_date_list(L),
-	length(L, Length),
-	get_all_courses(L, Length, CList),
+	get_all_courses(L, CList),
 	display("DAX", CList).
 	
-get_all_courses(_, 0, []). 
-get_all_courses(L, Length, CList) :-
-	Length > 0,
-	NewLength is Length - 1,
+get_all_courses([], []). 
+get_all_courses(L, CList) :-
 	head(Head, L),
 	tail(L, Tail),
 	dax(Head, EKurs, SKurs),
 	append([EKurs / 700], [SKurs / 700], DaxPart),
-	get_all_courses(Tail, NewLength, NewList),
-	append(NewList, DaxPart, XList),
-	CList = XList.
+	get_all_courses(Tail, NewList),
+	append(NewList, DaxPart, CList).	
+
+%1.2
+print_all_avg_courses(N) :-
+	get_avg_courses(N, R),
+	display("AVG", R).
+
+get_avg_courses(N, R) :-
+	get_date_list(L),
+	length(L, Length),
+    N =< Length,
+	get_all_avgs(L, N, R).
 	
-%get_AVG(L, 
-
-
+get_all_avgs(L, N, []) :-  length(L, Length), Length < N.
+get_all_avgs(L, N, R) :-
+	sublist(L, N, Sublist),
+	get_all_courses(Sublist, M),
+	get_AVG(M, AVG),
+	tail(L, Tail),
+	get_all_avgs(Tail, N, R2),
+	append(R2, [AVG], R).
+	
+get_AVG(L, R) :-
+	sumlist(L, Sum),
+	length(L, Length),
+	R is Sum / Length.
 
 sublist(L,N,P) :- 
 	append(P,_,L),
