88fdf81669ce029fd531bea10f60fcc354bd7ff8
Dewin <dewin18@live.de>
2018-01-19 13:52:59 +0100

    start ex. sheet 11

---
+++ 11/Blatt11_DeMaCh.pl
@@ -0,0 +1,21 @@
+/* SE3 LP - Aufgabenblatt 11
+
+Autor: 
+Dewin Bagci, 6815336
+Christian Cyll, 6870744
+Max Wutz, 6308876
+
+Datum 19.01.2018
+*/
+
+:- consult('translations.pl').
+
+%translate(?ListOfGermanWords, ?ListOfEnglischWords)
+%translate(GerList, EngList) :- 
+	
+
+
+
+
+
+	
---
+++ 11/translations.pl
@@ -0,0 +1,26 @@
+%Personalpronomen	
+translation(er, he).	
+translation(sie, she).	
+translation(es, it).
+
+%Artikel
+translation(ein, a).
+translation(eine, a).
+translation(der, the).
+translation(die, the).
+translation(das, the).
+
+%Nomen
+translation(beruf, career).
+translation(essen, food).
+translation(haus, house).
+
+%Verben
+translation(macht, makes).
+translation(geht, goes).
+translation(lacht, laughs).
+
+%Adjektive
+translation(schoen, nice).
+translation(nett, nice).
+translation(dumm, stupid).
